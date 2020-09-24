f56b2eaac6e0722af4030cb46924d61792c56823
zzzxxx00019 <zzzxxx00019@gmail.com>
2020-09-24 15:31:45 +0800

    add part2

---
+++ part2.c
@@ -0,0 +1,34 @@
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+
+uint64_t hexchar2val(char in[])
+{
+    int i=0, in_len = strlen(in) ;
+    uint64_t result=0 ;
+    
+    if(in_len>2 && in[0]=='0' && in[1]=='x') i = 2 ;
+
+    while(i < in_len)
+    {
+        const uint8_t letter = in[i] & 0x40;
+	const uint8_t shift = (letter >> 3) | (letter >> 6);
+
+	result = (result << 4) | ((in[i] + shift) & 0xf) ;
+	//printf("%1d\n",result) ;
+
+	i++ ;
+    }
+    return result ;
+
+}
+
+int main()
+{
+    printf("%ld\n",hexchar2val("0x00BAB10C")) ;
+
+    return 0 ;
+}
+
